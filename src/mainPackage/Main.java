a78a2e4a8d0240e45df10a0025c4fabe9e7c5eda
John Ericksen <bluephonix8@gmail.com>
2022-05-29 14:30:18 -0400

    starting working on UI stuff

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -4,12 +4,12 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		 Window window = new Window();
-		 window.activateTutorial();
-		 GameCalander calander = new GameCalander();
-		 EventCardStorage.initialize();
-		 calander.startGame(0);
+		  
 		 
+			/*
+			 * GameCalander calander = new GameCalander(); EventCardStorage.initialize();
+			 * calander.startGame(0);
+			 */
 		 
 	}
 
---
+++ src/mainPackage/TutorialWindow.java
@@ -0,0 +1,15 @@
+package mainPackage;
+
+import javax.swing.JPanel;
+
+public class TutorialWindow extends JPanel
+{
+
+	/**
+	 * Create the panel.
+	 */
+	public TutorialWindow() {
+
+	}
+
+}
